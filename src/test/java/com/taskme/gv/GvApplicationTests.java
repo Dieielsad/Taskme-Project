edfbd79bfe0c60dd75e3b46c38c6cc8c0c45b48d
Victor Saccucci <victorsaccucciv@gmail.com>
2024-01-05 17:39:53 -0300

    Primeiro commit

---
+++ src/test/java/com/taskme/gv/GvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskme.gv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
